bac841301393f1c325eed242ee050743ab7abbe7
Keith Cirkel <keithamus@users.noreply.github.com>
2020-07-30 12:22:54 +0100

    feat: export all members

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+export {StampedTemplate, Part} from './stamped-template'
+export {parse} from './template-string-parser'
+export {AttributeValuePart, AttributeValue} from './attribute-value'
+export type {StampedTemplateProcessor} from './stamped-template'
