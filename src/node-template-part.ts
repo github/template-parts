50453cfdc23928712b2dacc42223e578489f3caf
Keith Cirkel <keithamus@users.noreply.github.com>
2020-08-12 17:12:30 +0100

    refactor(NodeTemplatePart): add value/previousSibling/nextSibling API
    This aligns us closer to the spec

--- src/node-template-part.ts
+++ src/node-template-part.ts
@@ -1,9 +1,34 @@
 export class NodeTemplatePart {
-  constructor(public parentNode: ChildNode, public expression: string) {}
+  #parts: ChildNode[]
 
-  replaceWith(node: string | ChildNode): void {
-    if (typeof node === 'string') node = new Text(node)
-    this.parentNode.replaceWith(node)
-    this.parentNode = node
+  constructor(node: ChildNode, public expression: string) {
+    this.#parts = [node]
+  }
+
+  get value(): string {
+    return this.#parts.map(node => node.textContent).join('')
+  }
+
+  set value(string: string) {
+    this.replace(string)
+  }
+
+  get previousSibling(): ChildNode | null {
+    return this.#parts[0].previousSibling
+  }
+
+  get nextSibling(): ChildNode | null {
+    return this.#parts[this.#parts.length - 1].nextSibling
+  }
+
+  replace(...nodes: Array<string | ChildNode>): void {
+    const parts: ChildNode[] = nodes.map(node => {
+      if (typeof node === 'string') return new Text(node)
+      return node
+    })
+    const node = this.#parts.shift()!
+    node.replaceWith(...parts)
+    for (const part of this.#parts) part.remove()
+    this.#parts = parts
   }
 }
--- src/processors.ts
+++ src/processors.ts
@@ -1,12 +1,16 @@
 import type {TemplatePart} from './stamped-template.js'
-import {AttributeValueSetter} from './attribute-template-part.js'
+import {AttributeTemplatePart} from './attribute-template-part.js'
 
 export function propertyIdentity(parts: Iterable<TemplatePart>, params: Record<string, unknown>): void {
   for (const part of parts) {
     const key = part.expression
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     const value: any = key in params ? params[key] : ''
-    part.replaceWith(value)
+    if (part instanceof AttributeTemplatePart) {
+      part.replaceWith(value)
+    } else {
+      part.replace(value)
+    }
   }
 }
 
@@ -18,17 +22,18 @@ export function propertyIdentityOrBooleanAttribute(
     const key = part.expression
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     const value: any = key in params ? params[key] : ''
-    const attributeParentPart = part.parentNode
-    if (attributeParentPart instanceof AttributeValueSetter && attributeParentPart?.partList.length === 1) {
-      const element = attributeParentPart.element
-      const attr = attributeParentPart.parentNode
+    if (part instanceof AttributeTemplatePart && part.parentNode.partList.length === 1) {
+      const element = part.parentNode.element
+      const attr = part.parentNode.parentNode
       if (value === false) {
         element.removeAttribute(attr.name)
       } else {
         element.setAttribute(attr.name, value === true ? attr.name : value)
       }
-    } else {
+    } else if (part instanceof AttributeTemplatePart) {
       part.replaceWith(value)
+    } else {
+      part.replace(value)
     }
   }
 }
