bfe914e48f8a03118a0fc1e7d580908180a9f2fd
Keith Cirkel <keithamus@users.noreply.github.com>
2020-08-12 17:10:33 +0100

    refactor(NodeTemplatePart): drop useless attribute getter

--- src/node-template-part.ts
+++ src/node-template-part.ts
@@ -1,10 +1,6 @@
 export class NodeTemplatePart {
   constructor(public parentNode: ChildNode, public expression: string) {}
 
-  get attribute(): Attr | null {
-    return null
-  }
-
   replaceWith(node: string | ChildNode): void {
     if (typeof node === 'string') node = new Text(node)
     this.parentNode.replaceWith(node)
