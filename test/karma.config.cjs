6f5c556365eedad820fe8072b20ad60e3ea71f65
Keith Cirkel <keithamus@users.noreply.github.com>
2020-08-24 17:05:37 +0100

    build: use karma-safarinative-launcher

--- test/karma.config.cjs
+++ test/karma.config.cjs
@@ -10,7 +10,7 @@ module.exports = function (config) {
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
-    browsers: process.platform === 'darwin' ? ['ChromeHeadless', 'Safari'] : ['ChromeHeadless'],
+    browsers: process.platform === 'darwin' ? ['ChromeHeadless', 'SafariNative'] : ['ChromeHeadless'],
     autoWatch: false,
     singleRun: true,
     concurrency: Infinity
