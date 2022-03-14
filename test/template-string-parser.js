2e1a7317db811a3705984d541548c7166c1df0c3
Keith Cirkel <keithamus@users.noreply.github.com>
2022-03-14 15:07:48 +0000

    refactor parser to catch many edge cases

--- test/template-string-parser.js
+++ test/template-string-parser.js
@@ -1,48 +1,155 @@
 import {parse} from '../lib/template-string-parser.js'
+chai.config.truncateThreshold = Infinity
 
-describe('template-string-parser', () => {
-  it('extracts `{{}}` surrounding parts as part tokens', () => {
-    expect(Array.from(parse('{{x}}'))).to.eql([{type: 'part', start: 0, end: 5, value: 'x'}])
+function parserTest(message, ...tests) {
+  describe(message, () => {
+    for (let i = 0; i < tests.length; i += 2) {
+      it(tests[i], () => {
+        const output = parse(tests[i])
+        let n = 0
+        for (const token of output) {
+          expect(token)
+            .to.have.property('type')
+            .match(/^(part|string)$/)
+          expect(token).to.have.property('start', n)
+          expect(token).to.have.property('end').gte(n)
+          n = token.end
+        }
+        expect(output).to.eql(tests[i + 1])
+      })
+    }
   })
+}
 
-  it('tokenizes a template string successfully', () => {
-    expect(Array.from(parse('hello {{x}}'))).to.eql([
-      {type: 'string', start: 0, end: 6, value: 'hello '},
-      {type: 'part', start: 6, end: 11, value: 'x'}
-    ])
-  })
+describe.only('template-string-parser', () => {
+  parserTest('extracts `{{}}` surrounding parts as part tokens', '{{x}}', [
+    {type: 'part', start: 0, end: 5, value: 'x'}
+  ])
 
-  it('does not turn escaped `{{`s into expression tokens', () => {
-    expect(Array.from(parse('\\{{x}}'))).to.eql([{type: 'string', start: 0, end: 6, value: '\\{{x}}'}])
-  })
+  parserTest('tokenizes a template string successfully', 'hello {{x}}', [
+    {type: 'string', start: 0, end: 6, value: 'hello '},
+    {type: 'part', start: 6, end: 11, value: 'x'}
+  ])
 
-  it('does not terminate expressions with escaped `}}`s', () => {
-    expect(Array.from(parse('{{x\\}}}'))).to.eql([{type: 'part', start: 0, end: 7, value: 'x\\}'}])
-    expect(Array.from(parse('{{x\\}\\}}}'))).to.eql([{type: 'part', start: 0, end: 9, value: 'x\\}\\}'}])
-  })
-
-  it('strips leading and trailing whitespace', () => {
-    expect(Array.from(parse('{{ x }}'))).to.eql([{type: 'part', start: 0, end: 7, value: 'x'}])
-  })
-
-  it('tokenizes multiple values', () => {
-    expect(Array.from(parse('hello {{x}} and {{y}}'))).to.eql([
+  parserTest(
+    'tokenizes multiple values',
+    'hello {{x}} and {{y}}',
+    [
       {type: 'string', start: 0, end: 6, value: 'hello '},
       {type: 'part', start: 6, end: 11, value: 'x'},
       {type: 'string', start: 11, end: 16, value: ' and '},
       {type: 'part', start: 16, end: 21, value: 'y'}
-    ])
-  })
-
-  it('ignores single braces', () => {
-    expect(Array.from(parse('hello ${world?}'))).to.eql([{type: 'string', start: 0, end: 15, value: 'hello ${world?}'}])
-  })
-
-  it('ignores mismatching parens, treating them as text', () => {
-    expect(Array.from(parse('hello {{'))).to.eql([
+    ],
+    'hello {{x}} and {{y}}!',
+    [
       {type: 'string', start: 0, end: 6, value: 'hello '},
-      {type: 'string', start: 6, end: 8, value: '{{'}
-    ])
-    expect(Array.from(parse('hello }}'))).to.eql([{type: 'string', start: 0, end: 8, value: 'hello }}'}])
-  })
+      {type: 'part', start: 6, end: 11, value: 'x'},
+      {type: 'string', start: 11, end: 16, value: ' and '},
+      {type: 'part', start: 16, end: 21, value: 'y'},
+      {type: 'string', start: 21, end: 22, value: '!'}
+    ]
+  )
+
+  parserTest(
+    'tokenizes consecutive values',
+    'hello {{x}}{{y}}',
+    [
+      {type: 'string', start: 0, end: 6, value: 'hello '},
+      {type: 'part', start: 6, end: 11, value: 'x'},
+      {type: 'part', start: 11, end: 16, value: 'y'}
+    ],
+    '{{x}}{{y}}{{z}}',
+    [
+      {type: 'part', start: 0, end: 5, value: 'x'},
+      {type: 'part', start: 5, end: 10, value: 'y'},
+      {type: 'part', start: 10, end: 15, value: 'z'}
+    ],
+    'abc{{def}}{{ghi}}{{jkl}}{{mno}}{{pqr}}{{stu}}vwxyz',
+    [
+      {type: 'string', start: 0, end: 3, value: 'abc'},
+      {type: 'part', start: 3, end: 10, value: 'def'},
+      {type: 'part', start: 10, end: 17, value: 'ghi'},
+      {type: 'part', start: 17, end: 24, value: 'jkl'},
+      {type: 'part', start: 24, end: 31, value: 'mno'},
+      {type: 'part', start: 31, end: 38, value: 'pqr'},
+      {type: 'part', start: 38, end: 45, value: 'stu'},
+      {type: 'string', start: 45, end: 50, value: 'vwxyz'}
+    ]
+  )
+
+  parserTest('does not turn escaped `{{`s into expression tokens', '\\{{x}}', [
+    {type: 'string', start: 0, end: 6, value: '\\{{x}}'}
+  ])
+
+  parserTest('strips leading and trailing whitespace', '{{ x }}', [{type: 'part', start: 0, end: 7, value: 'x'}])
+
+  parserTest(
+    'correctly deals with trailing strings',
+    '{{ x }}!',
+    [
+      {type: 'part', start: 0, end: 7, value: 'x'},
+      {type: 'string', start: 7, end: 8, value: '!'}
+    ],
+    'hello {{x}}!',
+    [
+      {type: 'string', start: 0, end: 6, value: 'hello '},
+      {type: 'part', start: 6, end: 11, value: 'x'},
+      {type: 'string', start: 11, end: 12, value: '!'}
+    ]
+  )
+
+  parserTest(
+    'does not terminate expressions with escaped `}}`s',
+    '{{x\\}}}',
+    [{type: 'part', start: 0, end: 7, value: 'x\\}'}],
+    '{{x\\}\\}}}',
+    [{type: 'part', start: 0, end: 9, value: 'x\\}\\}'}],
+    '{{x\\}}',
+    [{type: 'string', start: 0, end: 6, value: '{{x\\}}'}],
+    '\\{{x}}',
+    [{type: 'string', start: 0, end: 6, value: '\\{{x}}'}]
+  )
+
+  parserTest('ignores single braces', 'hello ${world?}', [
+    {type: 'string', start: 0, end: 15, value: 'hello ${world?}'}
+  ])
+
+  parserTest(
+    'ignores mismatching parens, treating them as text',
+    'hello }}',
+    [{type: 'string', start: 0, end: 8, value: 'hello }}'}],
+    'hello {{',
+    [{type: 'string', start: 0, end: 8, value: 'hello {{'}],
+    'hello {{{{',
+    [{type: 'string', start: 0, end: 10, value: 'hello {{{{'}],
+    'hello {{}{',
+    [{type: 'string', start: 0, end: 10, value: 'hello {{}{'}],
+    'hello }}{{}',
+    [{type: 'string', start: 0, end: 11, value: 'hello }}{{}'}]
+  )
+
+  parserTest('ignores nested parens, treating them as text', '{{ "Your balance: {{ balance }}" }}', [
+    {type: 'part', start: 0, end: 35, value: '"Your balance: {{ balance }}"'}
+  ])
+
+  parserTest(
+    'parses awkward inputs correctly',
+    '{x{{}}}',
+    [
+      {type: 'string', start: 0, end: 2, value: '{x'},
+      {type: 'part', start: 2, end: 6, value: ''},
+      {type: 'string', start: 6, end: 7, value: '}'}
+    ],
+    '{{{x}}}',
+    [
+      {type: 'part', start: 0, end: 6, value: '{x'},
+      {type: 'string', start: 6, end: 7, value: '}'}
+    ],
+    '{}{{x}}}',
+    [
+      {type: 'string', start: 0, end: 2, value: '{}'},
+      {type: 'part', start: 2, end: 7, value: 'x'},
+      {type: 'string', start: 7, end: 8, value: '}'}
+    ]
+  )
 })
