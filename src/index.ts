8e8475790d7bb6d69d07449fc50965d8ebadeff7
Sean Doyle <sean.p.doyle24@gmail.com>
2024-12-13 23:29:39 -0500

    Export `InnerTemplatePart`
    Add an entry to the `src/index.ts` to export the new
    `InnerTemplatePart`.
    
    Since consumers are most likely to import `TemplateInstance`, change the
    `test/template-instance.ts` test file to import properties from the
    module in a way that more closely resembles the end-user consumer
    experience.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 export {TemplateInstance} from './template-instance.js'
 export {parse} from './template-string-parser.js'
 export {AttributeTemplatePart, AttributeValueSetter} from './attribute-template-part.js'
+export {InnerTemplatePart} from './inner-template-part.js'
 export {NodeTemplatePart} from './node-template-part.js'
 export {
   createProcessor,
